0ac6c682eedd228d526db472e7b44462ae99f6ce
arthursl12 <arthursoutolima@gmail.com>
2021-01-03 10:34:56 -0300

    Correção pasta dos testes

--- tests/run-test_art.sh
+++ tests/run-test_art.sh
@@ -7,8 +7,8 @@ for N in $(seq 1 1); do
     # Leave server window open during debug (close with C-b :kill-pane)
     # tmux set-window-option remain-on-exit on
     for K in $(seq 1 2); do 
-        tmux split-pane -v -c $(pwd)/ ./servidor 51511
-        ./client.py --script test$N-script$K.txt
+        tmux split-pane -v -c $(pwd)/../ ./servidor 51511
+        ../client.py --script test$N-script$K.txt
         while [[ -e test$N-output$K.txt ]] ; do
             if ! diff test$N-output$K.txt output$K.txt > /dev/null ; then
                 echo "test$N failed"
@@ -20,7 +20,7 @@ for N in $(seq 1 1); do
         echo "test$N.$K passed"
     done
     echo "test$N passed"
-    rm -rf client.log output*.txt
+    rm -rf ../client.log output*.txt ../*.swp
 done
 
 
